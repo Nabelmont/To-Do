a046946edcd7e179128fac3ab5a4f31bb9aa782e
Noah <nabelmont@me.com>
2017-12-06 20:45:00 -0800

    Added Bootstrap and Cleaned up HTML

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var submitBtn = document.querySelector("#submit");
+
+submitBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+  var submit = document.querySelector("#todoList");
+
+});
