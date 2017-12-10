aa9e8786b5894b1c11c966f8593f29b2177b576f
Noah <nabelmont@me.com>
2017-12-10 10:13:20 -0800

    Html & css focus n js

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ submitBtn.addEventListener('click', function(e) {
   e.preventDefault();
   var input = document.querySelector("#todoItem");
   var submit = document.querySelector("#todoList");
-  submit.append(input);
+  submit.appendChild(input);
 
 });
 //Need to get input to go to ul
