ed12240cb1ea9e4c108e53f34268a8415d1ce73b
Noah <nabelmont@me.com>
2017-12-11 21:03:53 -0800

    final push of the night

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,15 @@
 //Queries
 var submitBtn = document.querySelector('#submit');
-var input = document.getElementById('input');
+var input = document.getElementById('input').value;
 var display = document.querySelector('#todoList');
-var todo = [];
-var completed = [];
+
 
 submitBtn.addEventListener('click', function(e) {
   e.preventDefault();
   var li = document.createElement('li');
   display.appendChild(li);
-  li.appendChild(todo);
-  todo.appendChild(input);
+  textNode = document.createTextNode(input + ' ');
+  li.appendChild(input);
 
 
 });
-
-
-
-// function newButton() {
-//   var btn = document.createElement('button');
-//   btn.textContent = "Done";
-//
-// }
