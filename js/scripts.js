f1e1960423f3a3fe9f80776fd5162048f3d25e24
Noah <nabelmont@me.com>
2017-12-13 19:09:01 -0800

    finally got this ish to workkkkk

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,22 @@
 //Queries
 var addBtn = document.getElementById('add');
 var list = document.getElementById('list');
-
-add.onclick = function() {
+var doneList = document.getElementById('doneList');
+var removeButton = document.getElementById('remove');
+var textNode = document.getElementById('')
+// add function
+addBtn.onclick = function(e) {
+  e.preventDefault();
   addLi(list);
 }
 
+// function that adds li to list as well as a done button
 function addLi(targetUi) {
   var inputText = document.getElementById('input').value;
   var li = document.createElement('li');
   var textNode = document.createTextNode(inputText + " ");
   var removeButton = document.createElement('button');
-  document.getElementById('input').value = "";
+console.log(textNode);
 
   if(inputText.length === 0) {
     alert("You didn't type anything");
@@ -19,11 +24,11 @@ function addLi(targetUi) {
   }
 
   removeButton.className = "remove";
-  removeButton.innerHTML = " Done!";
+  removeButton.innerHTML = " Completed!";
   removeButton.setAttribute("onclick", "remove(this);");
 
   li.appendChild(textNode);
   li.appendChild(removeButton);
 
   targetUi.appendChild(li);
-  }
+}
